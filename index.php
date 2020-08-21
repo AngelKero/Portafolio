a6e4df226f7ec97703dde3582449a1a7722b8291
Angel Zaragoza <zangelweb@gmail.com>
2020-08-21 06:06:46 -0500

    Iniciando repositorio

---
+++ index.php
@@ -0,0 +1,102 @@
+<!doctype html>
+<html lang="es">
+    <head>
+        <title>Angel Zaragoza || Desarrollo WEB</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <!-- Fontasome CSS -->
+        <script defer src="https://use.fontawesome.com/releases/v5.1.1/js/all.js" integrity="sha384-BtvRZcyfv4r0x/phJt9Y9HhnN5ur1Z+kZbKVgzVBAlQZX4jvAuImlIz+bG7TS00a" crossorigin="anonymous"></script>
+        <!-- Google Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700|Oxanium:300,400,700&display=swap" rel="stylesheet">
+        <!-- Styles CSS -->
+        <link rel="stylesheet" href="sass/master.css">
+    </head>
+    <body>
+        <header>
+            <div class="container">
+                <nav class="row">
+                    <ul class="col-12">
+                        <li><a href="index.html">Inicio</a></li>
+                        <li><a href="blog.html">Blog</a></li>
+                        <li><a href="#">Acerca de...</a></li>
+                        <li><a href="#">Trabajos</a></li>
+                        <li><a href="#">Contacto</a></li>
+                        <li><a href="#">Mas informacion</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="contenedor-texto">
+                <div class="texto">
+                    <h1 class="nombre">Angel Zaragoza</h1>
+                    <h2 class="profesion">Desarrollo Web</h2>
+                </div>
+            </div>
+            <!-- Efecto Ola del header -->
+            <div class="wave" style="height: 150px; overflow: hidden;" ><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;"><path d="M0.00,49.98 C150.00,150.00 349.20,-50.00 500.00,49.98 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill: rgb(255, 255, 255);"></path></svg></div>
+        </header>
+        <main>
+            <section class="container acerca-de">
+                <div class="row foto">
+                    <div class="col-12">
+                        <img src="img/profile.jpg" alt="Foto de perfil - Angel Zaragoza">
+                    </div>
+                </div>
+                <div class="row texto">
+                    <h3 class="col-12 titulo">Acerca de</h3>
+                    <p class="col-12">This is a <span class="bold">Web's Version</span> of Lorem ipsum, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                        dolore eu fugiat nulla pariatur. Excepteur sint occaecat <span class="bold">cupidatat</span> non proident, sunt in culpa qui
+                        officia deserunt mollit anim id est laborum.
+                    </p>
+                </div>
+            </section>
+            <section class="redes-sociales bg-dark">
+                <div class="container">
+                    <!-- Titulo -->
+                    <div class="row">
+                        <div class="col-12">
+                            <h3 class="titulo mt-4 text-white">Redes Sociales</h3>
+                        </div>
+                    </div>
+                    <!-- Redes -->
+                    <div class="row lista text-white text-center">
+                        <!-- Cliente -->
+                        <div class="col-6 col-md-3 cliente">
+                            <i class="fab fa-facebook fa-lg fa-fw"></i>
+                            <p>Facebook</p>
+                        </div>
+                        <div class="col-6 col-md-3 cliente">
+                            <i class="fab fa-instagram fa-lg fa-fw"></i>
+                            <p>Instagram</p>
+                        </div>
+                        <div class="col-6 col-md-3 cliente">
+                            <i class="fab fa-github fa-lg fa-fw"></i>
+                            <p>GitHub</p>
+                        </div>
+                        <div class="col-6 col-md-3 cliente">
+                            <i class="fab fa-whatsapp fa-lg fa-fw"></i>
+                            <p>WhatsApp</p>
+                        </div>
+                        <div class="col-12 circle-rgb">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
+        
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    </body>
+</html>
